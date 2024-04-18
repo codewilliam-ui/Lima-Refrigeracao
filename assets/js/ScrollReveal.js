000b7ce6859920b161c3dec60690470c3880c2f9
Franklin William <ui.develloper@gmail.com>
2024-04-18 16:56:26 -0300

    Update

---
+++ assets/js/ScrollReveal.js
@@ -0,0 +1,54 @@
+window.revelar = ScrollReveal({ reset: true });
+
+/* Topo do Site */
+ScrollReveal().reveal('.img-topo-site',{
+    duration: 2000, /* duração do efeito */
+    distance: '90px',
+    delay: 500, /* tempo de atraso para começar o efeito */    
+});
+ScrollReveal().reveal('.txt-topo-site',{
+    duration: 2000,
+    distance: '90px'    
+});
+ScrollReveal().reveal('.btn',{
+    duration: 2000,
+    distance: '190px',    
+});
+ScrollReveal().reveal('.fa-phone',{
+    duration: 2000,
+    distance: '190px',    
+});
+
+/* Swiper */
+ScrollReveal().reveal('.mySwiper',{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    rotate: {
+        x: -100,
+    },
+});
+/* Nossos compromissos */
+ScrollReveal().reveal('.flex-intro',{
+    duration: 2000,
+    distance: '90px'    
+});
+ScrollReveal().reveal('.img-servicos',{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    rotate: {
+        x: 0,
+        y: 0,
+        z: 10
+    } 
+});
+
+/* Títulos das Seções */
+ScrollReveal().reveal('.titulo-secao',{
+    duration: 2000,
+    distance: '90px'    
+});
+
+
+
